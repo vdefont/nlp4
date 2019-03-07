311b05f09deb27b55aca9fb69989dbfed7ab77eb
vdefontnouvelle <vdefontnouvelle@CS219-17.local>
2019-03-06 21:58:27 -0800

    Done

--- code/nlp/parser/CKY.java
+++ code/nlp/parser/CKY.java
@@ -3,14 +3,8 @@ package nlp.parser;
 import java.util.*;
 import java.io.*;
 
-/**
-TODO:
-- Clean up massive "parseSentence" method
-- Test some more
-- Add extra credit 
-*/
-
-class Candidate {
+// Represents a candidate parse. To be inserted into cells of DP table.
+class Candidate implements Comparable<Candidate> {
   public String constituent;
   public double totalWeight;
   public Candidate leftChild;
@@ -29,25 +23,44 @@ class Candidate {
     this(s, d, null, null);
   }
 
-  public String toStringRec () {
+  public String toStringRec (boolean originalTree) {
+
     if (leftChild == null) return constituent;
-    else if (rightChild == null) return "(" + constituent + " " + leftChild.toStringRec() + ")";
-    else return "(" + constituent + " " + leftChild.toStringRec() + " " + rightChild.toStringRec() + ")";
+    // If unary rule
+    if (rightChild == null) return "(" + constituent + " " + leftChild.toStringRec(originalTree) + ")";
+
+    // Check if using a helper binary rule (ex. X23)
+    String childrenStr = leftChild.toStringRec(originalTree) + " " + rightChild.toStringRec(originalTree);
+    if (originalTree && constituent.charAt(0) == 'X') {
+      return childrenStr;
+    }
+    return "(" + constituent + " " + childrenStr + ")";
+
   }
 
-  public String toString () {
-    return toStringRec() + " " + totalWeight;
+  public String toString (boolean originalTree) {
+    return toStringRec(originalTree) + " " + totalWeight;
+  }
+
+  @Override
+  public int compareTo (Candidate b) {
+    Double aWeight = new Double(totalWeight);
+    Double bWeight = new Double(b.totalWeight);
+    return aWeight.compareTo(bWeight);
   }
 }
 
 public class CKY {
 
-  private static boolean DEBUG = false;
-
   private List<GrammarRule> lexicalRules, binaryRules;
   private Map<String,List<GrammarRule>> unaryRules; // rhs -> lhs
+  private boolean originalTree;
+  private int beamSize;
 
-  public CKY (String grammarFile, String sentencesFile) {
+  public CKY (String grammarFile, String sentencesFile, boolean originalTree, int beamSize) {
+
+    this.originalTree = originalTree;
+    this.beamSize = beamSize;
 
     lexicalRules = new ArrayList<>();
     unaryRules = new HashMap<>();
@@ -60,7 +73,7 @@ public class CKY {
 
   }
 
-  // Returns true if candidate added
+  // Tries adding a candidate to a cell, returns true if successfil
   private boolean addCandidateToCell (HashMap<String, Candidate> cell, Candidate newCand) {
     String constituent = newCand.constituent;
     // Add if cell doesn't contain this constituent, or if newCand is better
@@ -75,6 +88,7 @@ public class CKY {
     return addCell;
   }
 
+  // Adds all the unary rules for a new parse that's just been added to DP table
   private void addAllUnary (HashMap<String,Candidate> cell, Candidate cand) {
     String constituent = cand.constituent;
     if (unaryRules.containsKey(constituent)) {
@@ -88,25 +102,28 @@ public class CKY {
     }
   }
 
-  private String parseSentence (String sentence) {
+  // Ensures that a cell is under the size specified by beam search
+  private void beamLimit (HashMap<String,Candidate> cell) {
 
-    // Extract individual words
-    String[] words = sentence.split(" ");
-    int numWords = words.length;
-
-    // Set up grid of cells: each cell is a hashtable: constituent -> candidate
-    List<List<HashMap<String,Candidate>>> cells = new ArrayList<>();
-    for (int r = 0; r < numWords; r += 1) {
-      List<HashMap<String,Candidate>> row = new ArrayList<>();
-      for (int c = 0; c < numWords; c += 1) {
-        HashMap<String,Candidate> cell = new HashMap<>();
-        row.add(cell);
-      }
-      cells.add(row);
+    // Populate priority queue
+    PriorityQueue<Candidate> pq = new PriorityQueue<>();
+    for (String key : cell.keySet()) {
+      pq.add(cell.get(key));
     }
 
-    // Populate lexical rules
-    for (int i = 0; i < numWords; i += 1) {
+    // Clear map and insert best weight elements only
+    int top = Math.min(beamSize, cell.size());
+    cell.clear();
+    for (int i = 0; i < top; i += 1) {
+      Candidate c = pq.poll();
+      cell.put(c.constituent, c);
+    }
+  }
+
+  // Populates the diagonal of the DP table with all lexical rules
+  private void fillDpTableDiagonal (List<List<HashMap<String,Candidate>>> cells, String[] words) {
+    // Fill every cell
+    for (int i = 0; i < words.length; i += 1) {
       String word = words[i];
       HashMap<String, Candidate> cell = cells.get(i).get(i);
 
@@ -121,30 +138,31 @@ public class CKY {
           addCandidateToCell(cell, cand);
         }
       }
-    }
 
-    // Populate everything else
-    if (DEBUG) System.out.println();
-    if (DEBUG) System.out.println(numWords);
-    for (int diag = 1; diag < numWords; diag += 1) {
-      for (int row = 0; row < numWords - diag; row += 1) {
+      // In case of beam search
+      if (beamSize > 0) beamLimit(cell);
+    }
+  }
+
+  // Assumes the diagonal has already been filled in
+  // Fills in rest of DP table with candidate parses
+  private void fillDpTable (List<List<HashMap<String,Candidate>>> cells, String[] words) {
+    for (int diag = 1; diag < words.length; diag += 1) {
+      for (int row = 0; row < words.length - diag; row += 1) {
         int col = row + diag;
 
         HashMap<String,Candidate> curCell = cells.get(row).get(col);
 
-        if (DEBUG) System.out.println("Filling cell " + " (" + row + ", " + col + ")");
         // Check all pairs of children
         for (int childI = 0; childI < diag; childI += 1) {
           int childARow = row;
           int childACol = col - diag + childI;
           int childBRow = row + 1 + childI;
           int childBCol = col;
-          if (DEBUG) System.out.printf(" children: (%d, %d) - (%d, %d)\n", childARow, childACol, childBRow, childBCol);
 
           // Check all binary rules
           HashMap<String,Candidate> leftChild = cells.get(childARow).get(childACol);
           HashMap<String,Candidate> rightChild = cells.get(childBRow).get(childBCol);
-
           for (GrammarRule rule : binaryRules) {
             String lhs = rule.getLhs();
             ArrayList<String> rhs = rule.getRhs();
@@ -155,20 +173,40 @@ public class CKY {
               Candidate rCand = rightChild.get(rs);
               double newWeight = lCand.totalWeight + rCand.totalWeight + rule.getWeight();
               Candidate newCand = new Candidate(lhs, newWeight, lCand, rCand);
-              if (addCandidateToCell(curCell, newCand)) {
-                if (DEBUG) System.out.printf("  %s -> %s %s %s\n", lhs, ls, rs, "" + newWeight);
-              }
+              addCandidateToCell(curCell, newCand);
             }
-          }
+          } // Check all binary rules
+        } // Check all pairs of children
 
-        }
+        // If doing beam search
+        if (beamSize > 0) beamLimit(curCell);
       }
+    } // Fill every cell
+  }
+
+  private String parseSentence (String sentence) {
+
+    // Extract individual words
+    String[] words = sentence.split(" ");
+
+    // Set up grid of cells: each cell is a hashtable: constituent -> candidate
+    List<List<HashMap<String,Candidate>>> cells = new ArrayList<>();
+    for (int r = 0; r < words.length; r += 1) {
+      List<HashMap<String,Candidate>> row = new ArrayList<>();
+      for (int c = 0; c < words.length; c += 1) {
+        HashMap<String,Candidate> cell = new HashMap<>();
+        row.add(cell);
+      }
+      cells.add(row);
     }
 
+    fillDpTableDiagonal(cells, words);
+    fillDpTable(cells, words);
+
     // Print final sentence
-    HashMap<String,Candidate> rootCell = cells.get(0).get(numWords - 1);
+    HashMap<String,Candidate> rootCell = cells.get(0).get(words.length - 1);
     if (!rootCell.containsKey("S")) return "NULL";
-    else return rootCell.get("S").toString();
+    else return rootCell.get("S").toString(originalTree);
   }
 
   private void parseSentences (String sentencesFile) {
@@ -204,9 +242,28 @@ public class CKY {
   }
 
   public static void main (String[] args) {
-    String grammarFile = "../data/example.pcfg";
-    String sentencesFile = "../data/example.input";
-    new CKY(grammarFile, sentencesFile);
+
+    // Ensure appropriate args
+    String grammarFile = "";
+    String sentencesFile = "";
+    boolean originalTree = false;
+    int beamSize = 0;
+    for (int i = 0; i < args.length; i += 1) {
+      String arg = args[i];
+      if (arg.equals("-original")) {
+        originalTree = true;
+      } else if (arg.equals("-beam")) {
+        i += 1;
+        beamSize = Integer.valueOf(args[i]);
+      } else if (grammarFile.equals("")) {
+        grammarFile = arg;
+      } else {
+        sentencesFile = arg;
+      }
+    }
+
+    new CKY(grammarFile, sentencesFile, originalTree, beamSize);
+
   }
 
 }
---
+++ code/parse.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+java -cp . nlp.parser.CKY $1 $2 $3 $4 $5
